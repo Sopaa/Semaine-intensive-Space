c3ccc75b21f663f8ad3b7ae0d7a4dc71eee47a2d
SophieChieko <smemeint@gmail.com>
2018-06-27 15:42:44 +0200

    Ajout de la page de description

--- include/connexion.php
+++ include/connexion.php
@@ -6,7 +6,7 @@
  * Time: 13:01
  */
 try {
-	$conn = new PDO('mysql:host=localhost;port=3306;dbname=semaine_projet', 'root', 'root');
+	$conn = new PDO('mysql:host=localhost;port=3306;dbname=semaine_projet', 'root', 'riviere453!!!');
 } catch(PDOException $exception) {
 	die($exception -> getMessage());
 }
